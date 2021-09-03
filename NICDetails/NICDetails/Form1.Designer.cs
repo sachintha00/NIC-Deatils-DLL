4eb2b562fafbb84f538445d0b470d6cec4742d26
sachintha00 <sachintha0603@gmail.com>
2021-09-03 10:03:02 +0530

    updated

--- NICDetails/NICDetails/Form1.Designer.cs
+++ NICDetails/NICDetails/Form1.Designer.cs
@@ -28,13 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_birthyear = new System.Windows.Forms.Label();
+            this.lbl_month = new System.Windows.Forms.Label();
+            this.lbl_birthday = new System.Windows.Forms.Label();
+            this.lbl_fullbirthday = new System.Windows.Forms.Label();
+            this.lbl_gender = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.btn_GetDetail = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(119, 84);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(219, 26);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Enter Your NIC No:";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_input.Location = new System.Drawing.Point(74, 122);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(299, 26);
+            this.txt_input.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birth Year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Birth Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Birth Day";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 26);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Full Birthday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(56, 429);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 26);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Gender";
+            // 
+            // lbl_birthyear
+            // 
+            this.lbl_birthyear.AutoSize = true;
+            this.lbl_birthyear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_birthyear.Location = new System.Drawing.Point(270, 249);
+            this.lbl_birthyear.Name = "lbl_birthyear";
+            this.lbl_birthyear.Size = new System.Drawing.Size(38, 26);
+            this.lbl_birthyear.TabIndex = 0;
+            this.lbl_birthyear.Text = "00";
+            // 
+            // lbl_month
+            // 
+            this.lbl_month.AutoSize = true;
+            this.lbl_month.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_month.Location = new System.Drawing.Point(270, 294);
+            this.lbl_month.Name = "lbl_month";
+            this.lbl_month.Size = new System.Drawing.Size(38, 26);
+            this.lbl_month.TabIndex = 0;
+            this.lbl_month.Text = "00";
+            // 
+            // lbl_birthday
+            // 
+            this.lbl_birthday.AutoSize = true;
+            this.lbl_birthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_birthday.Location = new System.Drawing.Point(270, 339);
+            this.lbl_birthday.Name = "lbl_birthday";
+            this.lbl_birthday.Size = new System.Drawing.Size(38, 26);
+            this.lbl_birthday.TabIndex = 0;
+            this.lbl_birthday.Text = "00";
+            // 
+            // lbl_fullbirthday
+            // 
+            this.lbl_fullbirthday.AutoSize = true;
+            this.lbl_fullbirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_fullbirthday.Location = new System.Drawing.Point(270, 384);
+            this.lbl_fullbirthday.Name = "lbl_fullbirthday";
+            this.lbl_fullbirthday.Size = new System.Drawing.Size(38, 26);
+            this.lbl_fullbirthday.TabIndex = 0;
+            this.lbl_fullbirthday.Text = "00";
+            // 
+            // lbl_gender
+            // 
+            this.lbl_gender.AutoSize = true;
+            this.lbl_gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gender.Location = new System.Drawing.Point(270, 429);
+            this.lbl_gender.Name = "lbl_gender";
+            this.lbl_gender.Size = new System.Drawing.Size(38, 26);
+            this.lbl_gender.TabIndex = 0;
+            this.lbl_gender.Text = "00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(245, 249);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 26);
+            this.label11.TabIndex = 0;
+            this.label11.Text = ":";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(245, 294);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 26);
+            this.label12.TabIndex = 0;
+            this.label12.Text = ":";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(245, 339);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(19, 26);
+            this.label13.TabIndex = 0;
+            this.label13.Text = ":";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(245, 384);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(19, 26);
+            this.label14.TabIndex = 0;
+            this.label14.Text = ":";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(245, 429);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(19, 26);
+            this.label15.TabIndex = 0;
+            this.label15.Text = ":";
+            // 
+            // btn_GetDetail
+            // 
+            this.btn_GetDetail.Location = new System.Drawing.Point(74, 167);
+            this.btn_GetDetail.Name = "btn_GetDetail";
+            this.btn_GetDetail.Size = new System.Drawing.Size(106, 41);
+            this.btn_GetDetail.TabIndex = 2;
+            this.btn_GetDetail.Text = "Get Details";
+            this.btn_GetDetail.UseVisualStyleBackColor = true;
+            this.btn_GetDetail.Click += new System.EventHandler(this.btn_GetDetail_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(267, 167);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(106, 41);
+            this.btn_clear.TabIndex = 2;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(478, 494);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_GetDetail);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_gender);
+            this.Controls.Add(this.lbl_fullbirthday);
+            this.Controls.Add(this.lbl_birthday);
+            this.Controls.Add(this.lbl_month);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lbl_birthyear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_birthyear;
+        private System.Windows.Forms.Label lbl_month;
+        private System.Windows.Forms.Label lbl_birthday;
+        private System.Windows.Forms.Label lbl_fullbirthday;
+        private System.Windows.Forms.Label lbl_gender;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button btn_GetDetail;
+        private System.Windows.Forms.Button btn_clear;
     }
 }
 
--- NICDetails/NICDetails/Form1.cs
+++ NICDetails/NICDetails/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMDgetNicDetails;
 
 namespace NICDetails
 {
     public partial class Form1 : Form
     {
+        GetNicDetails nic;
+        String input;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_GetDetail_Click(object sender, EventArgs e)
+        {
+            input = txt_input.Text;
+            nic = new GetNicDetails(input);
+
+            lbl_birthyear.Text = nic.Year;
+            lbl_month.Text = nic.Month;
+            lbl_birthday.Text = nic.Day.ToString();
+            lbl_fullbirthday.Text = nic.BirthDay;
+            lbl_gender.Text = nic.Gender;
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            lbl_birthyear.Text = "";
+            lbl_month.Text = "";
+            lbl_birthday.Text = "";
+            lbl_fullbirthday.Text = "";
+            lbl_gender.Text = "";
         }
     }
 }
